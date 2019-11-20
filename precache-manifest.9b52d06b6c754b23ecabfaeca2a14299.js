8cacfc0b5a8dbf5355f3e7643db411d773642309
mstorino <maggiestorino@gmail.com>
2019-11-19 22:15:14 -0600

    Updates

---
+++ precache-manifest.9b52d06b6c754b23ecabfaeca2a14299.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c1bc4ba9213f83f2db7d0db11b364d9",
+    "url": "/playground/index.html"
+  },
+  {
+    "revision": "510c5d39ccf14aefab96",
+    "url": "/playground/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "54413de43e912fb914d7",
+    "url": "/playground/static/js/2.cbe1815f.chunk.js"
+  },
+  {
+    "revision": "510c5d39ccf14aefab96",
+    "url": "/playground/static/js/main.cb9dedc4.chunk.js"
+  },
+  {
+    "revision": "89694ef424b0ec5bcc4a",
+    "url": "/playground/static/js/runtime-main.1ee0a861.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/playground/static/media/logo.25bf045c.svg"
+  }
+]);
